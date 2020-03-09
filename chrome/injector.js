427f9529ab175c6dc5d38d885d186e16cf0d03f4
Kaj Schmidt <kaj.schmidt@ntig.se>
2020-03-09 09:20:27 +0100

    reenabled re-dress

--- chrome/injector.js
+++ chrome/injector.js
@@ -8,5 +8,10 @@
 	if (site_url.includes("student_ability")) { 
         $("body").append("<script language='Javascript' type='text/javascript' src='" + chrome.runtime.getURL("modules/course-list-fix/course-list-fix.js") + "'></script>");
     }
+
+    if (true) { 
+        $("body").append("<link rel='stylesheet' type='text/css' href='" + chrome.runtime.getURL("modules/re-dress/re-dress.css") + "'>");
+        $("body").append("<script language='Javascript' type='text/javascript' src='" + chrome.runtime.getURL("modules/re-dress/re-dress.js") + "'></script>");
+    }
     
 })();
